983ea250af1439679300f1f152f0a8f8e92eb83a
FabioDevGomes <fabio.dev.gomes@gmail.com>
2016-12-01 12:59:39 -0200

    Projeto inicial compilando

---
+++ spring-batch/src/com/fabio/CustomItemProcessor.java
@@ -0,0 +1,16 @@
+package com.fabio;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.fabio.model.Report;
+
+public class CustomItemProcessor implements ItemProcessor<Report, Report> {
+
+	@Override
+	public Report process(Report item) throws Exception {
+
+		System.out.println("Processing..." + item);
+		return item;
+	}
+
+}
